f67c0f737b21f44f06de2d30c84f2a933fbe6300
mattg3004 <matthewgraham3004@gmail.com>
2015-04-23 17:03:43 -0400

    First commit

---
+++ Hiv_sim.R
@@ -0,0 +1,63 @@
+
+##' Function idea - generate data using a Markov model, where there are two states and death. A person can only transition forwards i.e. state A to B is ok, but state B to A not allowed. There is a measurable variable, with both states having a different Normal distribution for this variable. 
+##' @param N - number of people
+##' @param num.data.points - number of data points to generate for each person
+##' @param Mean.1 - mean of the measurable variable for state A. Mean.2 is similar.
+##' @param SD.1 - standard deviation for measurable variable when in state A. SD.2 is similar.
+##' @param trans.A.to.B - probability of transitioning from state A to state B from one time point to another. trans.A.to.Death and trans.B.to.Death are similar.
+##' @param init.proportion.in.A - proportion of people who begin in state A
+
+
+generate.hidden.markov.model.data.2.states.plus.death <- function(N, num.data.points, Mean.1, SD.1, Mean.2, SD.2, trans.A.to.B, trans.A.to.Death, trans.B.to.Death, init.proportion.in.A){
+  
+  data = matrix(0, N * num.data.points, 3)
+  count = 1
+  for(i in 1 : N){
+    if(i < round((init.proportion.in.A * N))){
+      data[count, 2] = 1
+    } else {data[count, 2] = 2}
+    
+    for(j in 1 : num.data.points){
+      data[count, 1] = i
+      
+      if(j == 1){
+        state = data[count, 2]
+        if(state == 1){
+          data[count, 3] = max(0, rnorm(1, mean = Mean.1, sd = SD.1))
+        } else{data[count, 3] = max(0, rnorm(1, Mean.2, sd = SD.2))}
+      } else{
+        state = data[count - 1, 2]
+        if(state == 1){
+          k = runif(1)
+          if(k < trans.A.to.B){
+            data[count, 2] = 2
+            data[count, 3] = max(0, rnorm(1, Mean.2, sd = SD.2))  
+          } else if((k > trans.A.to.B) & (k < trans.A.to.B + trans.A.to.Death)){
+            data[count, 2] = 3
+          } else{
+            data[count, 2] = 1
+            data[count, 3] = max(0, rnorm(1, mean = Mean.1, sd = SD.1))  
+          }
+        } else if(state == 2){
+          k = runif(1)
+          if(k < trans.B.to.Death){
+            data[count, 2] = 3
+          } else{
+            data[count, 2] = 2
+            data[count, 3] = max(0, rnorm(1, Mean.2, sd = SD.2))  
+          }
+        } else{
+          data[count, 2] = 3
+        }
+        
+      }
+      count = count + 1
+    }
+  }
+  data = data.frame(data)
+  colnames(data) = c("ID", "State", "X")
+  return(data)
+}
+
+
+results = generate.hidden.markov.model.data.2.states.plus.death(N = 10, num.data.points = 5, Mean.1 = 700, SD.1 = 200, Mean.2 = 400, SD.2 = 200, trans.A.to.B = 0.3, trans.A.to.Death = 0.1, trans.B.to.Death = 0.2, init.proportion.in.A = 0.8)
