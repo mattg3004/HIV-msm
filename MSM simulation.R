72efd331991cefea0f16a556e72545cb54047ede
mattg3004 <matthewgraham3004@gmail.com>
2015-04-24 10:01:23 -0400

    Adding code that Kumi wrote

--- MSM simulation.R
+++ MSM simulation.R
@@ -1,14 +1,3 @@
-library(msm)
-
-##' Function idea - generate data using a Markov model, where there are two states and death. A person can only transition forwards i.e. state A to B is ok, but state B to A not allowed. There is a measurable variable, with both states having a different Normal distribution for this variable. 
-##' @param N - number of people
-##' @param num.data.points - number of data points to generate for each person
-##' @param Mean.1 - mean of the measurable variable for state A. Mean.2 is similar.
-##' @param SD.1 - standard deviation for measurable variable when in state A. SD.2 is similar.
-##' @param trans.A.to.B - probability of transitioning from state A to state B from one time point to another. trans.A.to.Death and trans.B.to.Death are similar.
-##' @param init.proportion.in.A - proportion of people who begin in state A
-
-
 generate.hidden.markov.model.data.2.states.plus.death <- function(N, num.data.points, Mean.1, SD.1, Mean.2, SD.2, trans.A.to.B, trans.A.to.Death, trans.B.to.Death, init.proportion.in.A){
   
   data = matrix(0, N * num.data.points, 4)
@@ -35,7 +24,6 @@ generate.hidden.markov.model.data.2.states.plus.death <- function(N, num.data.po
             data[count, 3] = max(0, rnorm(1, Mean.2, sd = SD.2))  
           } else if((k > trans.A.to.B) & (k < trans.A.to.B + trans.A.to.Death)){
             data[count, 2] = 3
-            data[count, 3] = -9
           } else{
             data[count, 2] = 1
             data[count, 3] = max(0, rnorm(1, mean = Mean.1, sd = SD.1))  
@@ -44,14 +32,12 @@ generate.hidden.markov.model.data.2.states.plus.death <- function(N, num.data.po
           k = runif(1)
           if(k < trans.B.to.Death){
             data[count, 2] = 3
-            data[count, 3] = -9
           } else{
             data[count, 2] = 2
             data[count, 3] = max(0, rnorm(1, Mean.2, sd = SD.2))  
           }
         } else{
           data[count, 2] = 3
-          data[count, 3] = -9
         }
         
       }
@@ -65,15 +51,29 @@ generate.hidden.markov.model.data.2.states.plus.death <- function(N, num.data.po
 }
 
 
-results = generate.hidden.markov.model.data.2.states.plus.death(N = 5000, num.data.points = 10, Mean.1 = 700, SD.1 = 200, Mean.2 = 400, SD.2 = 200, trans.A.to.B = 0.2, trans.A.to.Death = 0.1, trans.B.to.Death = 0.1, init.proportion.in.A = 0.7)
+results = generate.hidden.markov.model.data.2.states.plus.death(N = 100, num.data.points = 5, Mean.1 = 700, SD.1 = 200, Mean.2 = 400, SD.2 = 200, trans.A.to.B = 0.3, trans.A.to.Death = 0.1, trans.B.to.Death = 0.2, init.proportion.in.A = 0.8)
+
+results$X[which(results$X==0)]<- -9 #Kate set final state to this; doesn't appear to be necessary
 
 
-Q = rbind(c(0, 0.3, 0.1), 
-          c(0, 0, 0.2), 
-          c(0, 0, 0))
+statetable.msm(State, ID, data=results)
 
-a = msm(X ~ time, subject=ID, data = results,
-     qmatrix = Q,
-     hmodel = list (hmmNorm(mean=800, sd=150), hmmNorm(mean=450, sd=200),
-                    hmmIdent(-9)) )
-print(a)
+Q<-rbind(c(-.25, .25,  0),
+         c(0,   -.25,  .25),
+         c(0,     0,   1))
+
+Q.emp<-crudeinits.msm(State~time, ID, data=results, qmatrix=Q)
+
+statelist=list(hmmNorm(mean=mean(results$X[results$State==1]), sd=sd(results$X[results$State==1])),
+               hmmNorm(mean=mean(results$X[results$State==2]), sd=sd(results$X[results$State==2])),
+               hmmIdent(-9))
+hiv.hmm<-msm(X~time, subject=ID, data=results, qmatrix=Q.emp, hmodel=statelist)
+
+plot(hiv.hmm, legend.pos=c(3.5, 1)) #survival plots (survival=not entering a final absorbing state)
+pmatrix.msm(hiv.hmm) #transition probability matricies
+
+sojourn.msm(hiv.hmm) #mean sojourn times
+pnext.msm(hiv.hmm) #probability that each state is next (don't understand this one)
+totlos.msm(hiv.hmm) #total length of stay
+hazard.msm(hiv.hmm) #if we include covariates
+
